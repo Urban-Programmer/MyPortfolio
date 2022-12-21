8a4cc2cb31644ffe66cd4b5d9bd18a9a08d28a3f
Chris Wilson <awil17@outlook.com>
2022-12-21 04:03:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "#bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x7accfe,
+  backgroundColor: 0x010f18,
+});
